6aa3d97d4cf9956fbe690db6092b88d8a91b90bd
Janvi Patel <pateljanvi0328@gmail.com>
2023-03-14 11:55:51 +0530

    add axios interceptor

--- src/App.vue
+++ src/App.vue
@@ -20,11 +20,29 @@
 <script>
 import SideBar from "./components/SideBar.vue";
 import TopBar from "./components/TopBar.vue";
-
+import axios from "axios";
 export default {
   components: {
     SideBar,
     TopBar
+  },
+
+  created() {
+    axios.interceptors.response.use(
+      response => {
+        // Any status code that lie within the range of 2xx cause this function to trigger
+        // Do something with response data
+        return response;
+      },
+      error => {
+        // Any status codes that falls outside the range of 2xx cause this function to trigger
+        // Do something with response error
+        if (error.response.status == 401) {
+          this.$router.push("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
   }
 }
 </script>
